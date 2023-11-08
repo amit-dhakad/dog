2f436b09a5a5c105e6e2fc90905b4f88f1139175
amit dhakad <amit0502dhakad@gmail.com>
2023-11-08 15:51:45 +0530

    added dog module

---
+++ dog.go
@@ -0,0 +1,5 @@
+import "strings"
+
+func WhenGrownUp(s string) {
+	return " Wen the puppy grows up it says : " + strings.toUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amit-dhakad/dog
+
+go 1.21.3
